fccab9028af7fee7ef4e3816e0e364ed452e4300
Aqua <1076793590@qq.com>
2026-04-18 13:07:58 +0800

    Merge

--- services/doubao_summary_service.py
+++ services/doubao_summary_service.py
@@ -21,7 +21,7 @@ def load_dotenv_file() -> None:
         key, value = line.split("=", 1)
         key = key.strip()
         value = value.strip().strip("'\"")
-        if key and key not in os.environ:
+        if key and (key not in os.environ or not os.environ.get(key, "").strip()):
             os.environ[key] = value
 
 
